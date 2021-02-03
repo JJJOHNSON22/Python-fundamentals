f65a39eb0a3e989c12afafa86afc0fbe1267ab30
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-02-03 08:10:09 -0500

    Add string.isalnum() method

--- hello_world.py
+++ hello_world.py
@@ -6,7 +6,8 @@ num = 15
 print("Hello", num)
 print("Hello " + str(num))
 food_one = "pizza"
-food_two = "hot dog"
+food_two = "hot dogs"
 print("I love to eat {} and {}.".format(food_one, food_two))
 print(f'I love to eat {food_one} and {food_two}.')
-
+print(name.isalnum())
+print(food_two.isalnum())
