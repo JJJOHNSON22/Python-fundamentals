0151669285205f15cb1c1cf63775bf9a34c9dd16
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-02-04 02:01:18 -0500

    Made randInt function work properly

--- functions_intermediate1.py
+++ functions_intermediate1.py
@@ -1,25 +1,19 @@
 import random
 def randInt(max_num=0, min_num=0):
-    if (min_num==0 and max_num==0 or min_num > max_num or max_num < 0):
-        if (max_num < min_num):
-            num = int(random.uniform(max_num, min_num))
-            return num
-        else:
-            num = int(random.random() * 100)
-            return num
+    if (min_num==0 and max_num==0):
+        num = int(random.random() * 100)
+        return num
     elif (min_num!=0 and max_num!=0):
-        num = int(random.random() * max_num + min_num)
+        num = int(random.random() * (max_num - min_num) + min_num)
         return num
     elif (max_num!=0):
         num = int(random.random() * max_num)
         return num
     else:
-        num = int(random.random() * 100 + min_num)
+        num = int(random.random() * (100 - min_num) + min_num)
         return num
 
 print(randInt())
-print(randInt(max_num=-30))
-print(randInt(max_num=47))
-print(randInt(min_num=23))
-print(randInt(min_num=20, max_num=522))
-print(randInt(min_num=240, max_num=-58))
+print(randInt(max_num=50))
+print(randInt(min_num=50))
+print(randInt(min_num=50, max_num=500))
